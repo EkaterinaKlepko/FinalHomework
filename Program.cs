edccb369da6c3c90728ec4c6567d79850d4ddf96
ekaterina <kat_klepko@mail.ru>
2023-04-02 12:22:44 +0300

    commit3

--- Program.cs
+++ Program.cs
@@ -11,5 +11,14 @@
     }
 }
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
 string[] array1 = new string[5] {"1236", "23", "hel", "wow", "result"};
 string[] array2 = new string[array1.Length];
