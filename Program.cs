1e4bd401547164aacbe8df3fdbc9972cc1985af6
ekaterina <kat_klepko@mail.ru>
2023-04-02 12:22:08 +0300

    commit2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿string[] array1 = new string[5] {"1236", "23", "hel", "wow", "result"};
+﻿void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+string[] array1 = new string[5] {"1236", "23", "hel", "wow", "result"};
 string[] array2 = new string[array1.Length];
