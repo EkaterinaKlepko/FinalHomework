f57957126e9fabdace7afb32d8d13d935226e561
ekaterina <kat_klepko@mail.ru>
2023-04-02 12:21:00 +0300

    commit1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[5] {"1236", "23", "hel", "wow", "result"};
+string[] array2 = new string[array1.Length];
